38de89283e88a1014815e3267a006c8be73c87be
unknown <margarita.salazar.mls@gmail.com>
2021-06-09 01:20:03 -0500

    endpoint to get all cars

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,15 @@
+const db = require('../../data/db-config.js')
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db('cars')
 }
 
 const getById = () => {
-  // DO YOUR MAGIC
+
 }
 
 const create = () => {
-  // DO YOUR MAGIC
+
 }
+
+module.exports = { getAll, getById, create}
--- api/server.js
+++ api/server.js
@@ -1,7 +1,22 @@
 const express = require("express")
 
+const carsRouter = require('./cars/cars-router')
+
 const server = express()
 
-// DO YOUR MAGIC
+server.use(express.json())
+
+server.use('/api/cars', carsRouter)
+
+server.use('*', (req, res)=>{
+    res.send('<h>Its Working!!</h>')
+})
+
+server.use((err, req, res, next)=>{ // eslint-disable-line
+    res.status(err.status || 500).json({
+        custom: "Something went wrong",
+        message: err.message,
+      })
+})
 
 module.exports = server
